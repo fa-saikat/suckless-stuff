735974617ea84f4532165021815a80583c2f1aaf
FA Saikat <saikat.cucse.52@gmail.com>
2023-09-02 19:36:31 +0600

    Added new theme: Everblush

---
+++ dwm/themes/everblush.h
@@ -0,0 +1,18 @@
+static const char col_bar_bg[]  = "#141B1E";
+
+static const char col_nor_bor[] = "#232E2D";
+static const char col_act_bor[] = "#E57474";
+
+static const char col_tag_fg[]  = "#B3B9B8";
+static const char col_tag_bg[]  = "#141B1E";
+static const char col_tag_sel[] = "#E57474";
+
+static const char *colors[][3]  = {
+	[SchemeNorm] = { col_tag_fg , col_bar_bg, col_nor_bor },
+	[SchemeSel] = { col_tag_sel, col_tag_bg, col_act_bor },
+};
+
+static const char dmenu_bg[]  = "#141B1E";
+static const char dmenu_fg[]  = "#E57474";
+static const char dmenu_sbg[] = "#141B1E";
+static const char dmenu_sfg[] = "#8CCF7E";
