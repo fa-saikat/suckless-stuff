dd76bbe085d6a983f615817e46208a683bde0fe6
FA Saikat <saikat.cucse.52@gmail.com>
2023-09-28 10:20:42 +0600

    --

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -7,52 +7,65 @@ function run {
   fi
 }
 
-### Resoluiton & Monitor ###
+# ░█▀▄░▀█▀░█▀▀░█▀█░█░░░█▀█░█░█
+# ░█░█░░█░░▀▀█░█▀▀░█░░░█▀█░░█░
+# ░▀▀░░▀▀▀░▀▀▀░▀░░░▀▀▀░▀░▀░░▀░
 # run "xrandr --output DP2 --right-of eDP1 --auto"
 
-
-### Statusbar ###
+# ░█▀▀░█░█░█▀▀░▀█▀░█▀▄░█▀█░█░█
+# ░▀▀█░░█░░▀▀█░░█░░█▀▄░█▀█░░█░
+# ░▀▀▀░░▀░░▀▀▀░░▀░░▀░▀░▀░▀░░▀░
 bash ~/.local/src/dwm/bar/statusbar.sh &
 # run "slstatus"
 # run "numlockx on"
 # run "volumeicon"
-# run "nm-applet"
-# run "blueberry-tray"
+run "pa-applet"
+run "mictray"
+run "nm-applet"
+run "blueberry-tray"
+run "xfce4-clipman"
+# run "cbatticon -u 20 -i standard"
 # run "klipper"
-# ibus-daemon -drxR &
+ibus-daemon -drxR &
 
-
-### Notification deamon ###
+# ░█▀█░█▀█░▀█▀░▀█▀░█▀▀░▀█▀░█▀▀░█▀█░▀█▀░▀█▀░█▀█░█▀█
+# ░█░█░█░█░░█░░░█░░█▀▀░░█░░█░░░█▀█░░█░░░█░░█░█░█░█
+# ░▀░▀░▀▀▀░░▀░░▀▀▀░▀░░░▀▀▀░▀▀▀░▀░▀░░▀░░▀▀▀░▀▀▀░▀░▀
 # run "/usr/lib/xfce4/notifyd/xfce4-notifyd"
 run "dunst"
 
-
-### Power Manager ###
+# ░█▀█░█▀█░█░█░█▀▀░█▀▄
+# ░█▀▀░█░█░█▄█░█▀▀░█▀▄
+# ░▀░░░▀▀▀░▀░▀░▀▀▀░▀░▀
 run "xfce4-power-manager"
 
-
-### Polkit authentication agent ###
+# ░█▀█░█▀█░█░░░█░█░▀█▀░▀█▀
+# ░█▀▀░█░█░█░░░█▀▄░░█░░░█░
+# ░▀░░░▀▀▀░▀▀▀░▀░▀░▀▀▀░░▀░
 # run "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
 run "/usr/lib/polkit-kde-authentication-agent-1"
 
-
-### Wallpapers & Conkies ###
+# ░█▀▄░█▀▀░░░▄▀░░░░█▀▀░█▀█░█▀█░█░█░█░█
+# ░█▀▄░█░█░░░▄█▀░░░█░░░█░█░█░█░█▀▄░░█░
+# ░▀▀░░▀▀▀░░░░▀▀░░░▀▀▀░▀▀▀░▀░▀░▀░▀░░▀░
 run "nitrogen --restore"
 # run "conky -c $HOME/.config/dwm/conky/qlock_two.conkyrc"
 # run "conky -c $HOME/.local/src/dwm/conky/simple.conkyrc"
 
-
-### Compositor ###
+# ░█▀▀░█▀█░█▄█░█▀█░█▀█░█▀▀░▀█▀░▀█▀░█▀█░█▀▄
+# ░█░░░█░█░█░█░█▀▀░█░█░▀▀█░░█░░░█░░█░█░█▀▄
+# ░▀▀▀░▀▀▀░▀░▀░▀░░░▀▀▀░▀▀▀░▀▀▀░░▀░░▀▀▀░▀░▀
 picom --experimental-backends --config ~/.config/picom/picom.conf &
-
-
-### Keybinding ###
+#
+# ░█░█░█▀▀░█░█░█▀▄░▀█▀░█▀█░█▀▄░▀█▀░█▀█░█▀▀
+# ░█▀▄░█▀▀░░█░░█▀▄░░█░░█░█░█░█░░█░░█░█░█░█
+# ░▀░▀░▀▀▀░░▀░░▀▀░░▀▀▀░▀░▀░▀▀░░▀▀▀░▀░▀░▀▀▀
 sxhkd -c ~/.config/sxhkd/sxhkdrc &
 
 
 ### X11 ###
-xrdb -load ~/.config/X11/xresources &
+# xrdb -load ~/.config/X11/xresources &
 
 
 # imwheel -b 45 &
-qbittorrent &
+# qbittorrent &
